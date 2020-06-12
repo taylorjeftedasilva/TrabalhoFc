9333253a684faab2d1e705102373e5b35dfe32a6
Taylor <taylorjeftedasilva@gmail.com>
2020-06-11 21:00:51 -0300

    adição do arquivo javascript, para gerar de forma automática as <tr>s e <td>s

---
+++ js/agendamento.js
@@ -0,0 +1,44 @@
+var hora = document.querySelector('#agenda');
+count=10;
+while (count<=20){
+  tabela(count,'00');
+  if (count == 20 || count >24){
+    break;
+  }
+  else{
+    tabela(count,30);
+  }
+  count++;
+}
+function tabela(h, y){
+  var tr =document.createElement('tr');
+  var tdSEgunda= document.createElement('td');
+  var tdTerca =document.createElement('td');
+  var tdQuarta =document.createElement('td');
+  var tdQuinta= document.createElement('td');
+  var tdSexta= document.createElement('td');
+  var tdSabado= document.createElement('td');
+  var tdDomingo= document.createElement('td')
+  tdSEgunda.classList.add("segunda");
+  tdTerca.classList.add("terca");
+  tdQuarta.classList.add("quarta");
+  tdQuinta.classList.add("quinta");
+  tdSexta.classList.add("sexta");
+  tdSabado.classList.add("sabado");
+  tdDomingo.classList.add("domingo")
+  tdSEgunda.textContent=h+':'+y;
+  tdTerca.textContent=h+':'+y;
+  tdQuarta.textContent=h+':'+y;
+  tdQuinta.textContent=h+':'+y;
+  tdSexta.textContent=h+':'+y;
+  tdSabado.textContent=h+':'+y;
+  tdDomingo.textContent=h+':'+y;
+  hora.appendChild(tr);
+  tr.appendChild(tdSEgunda);
+  tr.appendChild(tdTerca);
+  tr.appendChild(tdQuarta);
+  tr.appendChild(tdQuinta);
+  tr.appendChild(tdSexta);
+  tr.appendChild(tdSabado);
+  tr.appendChild(tdDomingo);
+}
